c51c293accf388766b447e027354366076f0a428
MCollier24 <matthew.collier@outlook.com>
2025-01-12 03:09:46 -0500

    Added programmable load abstract class

---
+++ src/instruments/abstract_instruments/programmable_load.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+"""
+Provides an abstract base class for programmable load instruments
+"""
+
+# IMPORTS #####################################################################
+
+import abc
+
+from instruments.abstract_instruments import Instrument
+
+# CLASSES #####################################################################
+
+
+class ProgrammableLoad(Instrument, metaclass=abc.ABCMeta):
+    """
+    Abstract base class for programmable load instruments.
+
+    All applicable concrete instruments should inherit from this ABC to
+    provide a consistent interface to the user.
+    """
+
+    class Channel(metaclass=abc.ABCMeta):
+        """
+        Abstract base class for programmable load input channels.
+
+        All applicable concrete instruments should inherit from this ABC to
+        provide a consistent interface to the user.
+        """
+
+        # PROPERTIES #
+
+        @property
+        @abc.abstractmethod
+        def mode(self):
+            """
+            Gets/sets the input mode for the programmable load channel. This is an
+            abstract method.
+
+            :type: `~enum.Enum`
+            """
+
+        @mode.setter
+        @abc.abstractmethod
+        def mode(self, newval):
+            pass
+
+        @property
+        @abc.abstractmethod
+        def voltage(self):
+            """
+            Gets/sets the input voltage for the programmable load channel. This is an
+            abstract method.
+
+            :type: `~pint.Quantity`
+            """
+
+        @voltage.setter
+        @abc.abstractmethod
+        def voltage(self, newval):
+            pass
+
+        @property
+        @abc.abstractmethod
+        def current(self):
+            """
+            Gets/sets the input current for the programmable load channel. This is an
+            abstract method.
+
+            :type: `~pint.Quantity`
+            """
+
+        @current.setter
+        @abc.abstractmethod
+        def current(self, newval):
+            pass
+
+        @property
+        @abc.abstractmethod
+        def output(self):
+            """
+            Gets/sets the input status for the programmable load channel. This is an
+            abstract method.
+
+            :type: `bool`
+            """
+
+        @output.setter
+        @abc.abstractmethod
+        def output(self, newval):
+            pass
+
+    # PROPERTIES #
+
+    @property
+    @abc.abstractmethod
+    def channel(self):
+        """
+        Gets a channel object for the programmable load. This should use
+        `~instruments.util_fns.ProxyList` to achieve this.
+
+        This is an abstract method.
+
+        :rtype: `ProgrammableLoad.Channel`
+        """
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def voltage(self):
+        """
+        Gets/sets the input voltage for all channel on the programmable load.
+        This is an abstract method.
+
+        :type: `~pint.Quantity`
+        """
+
+    @voltage.setter
+    @abc.abstractmethod
+    def voltage(self, newval):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def current(self):
+        """
+        Gets/sets the input current for all channel on the programmable load.
+        This is an abstract method.
+
+        :type: `~pint.Quantity`
+        """
+
+    @current.setter
+    @abc.abstractmethod
+    def current(self, newval):
+        pass
